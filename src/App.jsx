030f54899e326c22199aefe6db84819c4f221056
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-05-19 19:43:00 +0500

    I have fixed in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
